df547e2805c54fbcd4ebc9323cdb49028e9bf13f
Betico1928 <betoveluis@gmail.com>
2022-05-15 19:48:16 -0500

    Initial Commit

---
+++ src/main/java/com/example/personappv2/controladorPersona.java
@@ -0,0 +1,59 @@
+package com.example.personappv2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class controladorPersona {
+
+    @FXML // fx:id="botonEliminar"
+    private Button botonEliminar; // Value injected by FXMLLoader
+
+    @FXML // fx:id="botonGuardar"
+    private Button botonGuardar; // Value injected by FXMLLoader
+
+    @FXML // fx:id="botonIrAlTelefono"
+    private Button botonIrAlTelefono; // Value injected by FXMLLoader
+
+    @FXML // fx:id="botonSalir"
+    private Button botonSalir; // Value injected by FXMLLoader
+
+    @FXML // fx:id="botonX"
+    private Button botonX; // Value injected by FXMLLoader
+
+    @FXML // fx:id="radioFemenino"
+    private RadioButton radioFemenino; // Value injected by FXMLLoader
+
+    @FXML // fx:id="radioMasculino"
+    private RadioButton radioMasculino; // Value injected by FXMLLoader
+
+    @FXML // fx:id="radioOtro"
+    private RadioButton radioOtro; // Value injected by FXMLLoader
+
+    @FXML // fx:id="textoApellido"
+    private TextField textoApellido; // Value injected by FXMLLoader
+
+    @FXML // fx:id="textoCedula"
+    private TextField textoCedula; // Value injected by FXMLLoader
+
+    @FXML // fx:id="textoContactos"
+    private TextArea textoContactos; // Value injected by FXMLLoader
+
+    @FXML // fx:id="textoNombre"
+    private TextField textoNombre; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tituloApellido"
+    private Text tituloApellido; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tituloCedula"
+    private Text tituloCedula; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tituloGenero"
+    private Text tituloGenero; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tituloNombre"
+    private Text tituloNombre; // Value injected by FXMLLoader
+}
